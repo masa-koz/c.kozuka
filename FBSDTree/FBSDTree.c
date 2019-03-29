4f2ae14dd33ff08e9cd27173acf23e85a556848f
Masahiro Kozuka <masa.koz@gmail.com>
2019-03-29 00:13:21 -0400

    Practice of using tree.h provided by FreeBSD project.

---
+++ FBSDTree/FBSDTree.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tree.h"
+
+struct MyData {
+	RB_ENTRY(MyData) entry;
+	int num;
+};
+
+int
+MyDataCompare(
+	struct MyData *a,
+	struct MyData *b
+)
+{
+	return a->num - b->num;
+}
+
+RB_HEAD(MyDataEntries, MyData) Head = RB_INITIALIZER(&Head);
+
+RB_PROTOTYPE(MyDataEntries, MyData, entry, MyDataCompare);
+RB_GENERATE(MyDataEntries, MyData, entry, MyDataCompare);
+
+int __cdecl
+main(void)
+{
+	int i, mod;
+	struct MyData *data;
+
+	mod = 0;
+Retry:
+	for (i = 0; i < 256; i++) {
+		if ((i % 3) != mod)
+			continue;
+		data = malloc(sizeof(struct MyData));
+		if (data != NULL) {
+			memset(data, 0, sizeof(struct MyData));
+			data->num = i;
+			RB_INSERT(MyDataEntries, &Head, data);
+			printf("%d,", data->num);
+		}
+	}
+	mod++;
+	if (mod < 3) {
+		goto Retry;
+	}
+	printf("\n");
+
+	RB_FOREACH(data, MyDataEntries, &Head) {
+		printf("%d,", data->num);
+	}
+	printf("\n");
+	return 0;
+}
